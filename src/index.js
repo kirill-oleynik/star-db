d1dfd38a82d2d8dc46531fabf945dad67ff8f42b
KirillØleynik <kirill.olejnik@gmail.com>
2020-10-20 19:04:35 +0300

    54. handling error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './bootstrap.min.css';
 
-const getResource = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((body) => { console.log(body.name) })
+class swapiService {
+  _apiBase = 'https://swapi.dev/api';
+  async getResource(url) {
+    fetch(`${this._apiBase}${url}`)
+    .then((response) => {
+      if(!response.ok){
+        console.log('COULD NOT FETCH')
+      }else{
+        return response.json()
+      }
+    })
+    .then((body) => {console.log(body)})
+  }
 }
-getResource('https://swapi.dev/api/people/1/')
+
+const apiService = new swapiService();
+apiService.getResource('/people/1/')
 ReactDOM.render(
   <React.StrictMode>
     <App />
