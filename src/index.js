80e29a617dc4c57833b9402696493914d00eb4c6
KirillØleynik <kirill.olejnik@gmail.com>
2020-10-20 17:17:12 +0300

    53. how fetch works (error if using await!)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const getResource = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((body) => { console.log(body.name) })
+}
+getResource('https://swapi.dev/api/people/1/')
 ReactDOM.render(
   <React.StrictMode>
     <App />
