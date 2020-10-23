bdfec0b9e1024632201dd4ce74e38a474196aed4
KirillØleynik <kirill.olejnik@gmail.com>
2020-10-23 12:12:36 +0300

    55. StarDB components

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './bootstrap.min.css';
-
-const App = () => (<h1>StarDB</h1>);
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './bootstrap.min.css';
-
+import App from './components/app';
 class swapiService {
   _apiBase = 'https://swapi.dev/api';
   async getResource(url) {
