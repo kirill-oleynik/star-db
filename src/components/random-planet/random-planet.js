7794c09a93037763b60975bb4c59e2ce83ff6b34
KirillØleynik <kirill.olejnik@gmail.com>
2020-10-24 21:22:52 +0300

    61. Handling network errors in component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -14,9 +14,11 @@ export default class RandomPlanet extends Component {
   onPlanetLoaded =(planet) => {this.setState({planet, loading: false})};
 
   state = {
+    error: false,
     loading: true,
     planet: {}
   }
+  _onError = (error) => { this.setState({ error: true, loading: false }) }
   planetId(max){
     return Math.floor(Math.random() * Math.floor(max));
   }
@@ -24,20 +26,27 @@ export default class RandomPlanet extends Component {
     const id = this.planetId(20);
     this.api.getPlanet(id)
     .then(this.onPlanetLoaded)
+    .catch(this._onError)
 
   }
   render() {
-    const { loading, planet } = this.state;
+    const { loading, planet, error } = this.state;
+    const hasData = !error && !loading;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet} /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
     return (
       <div className="random-planet jumbotron rounded">
       {spinner}
       {content}
-      
+      {errorMessage}
+
       </div>
 
     );
   }
 }
 
+const ErrorMessage = () => {
+  return(<p>404 Not Found :(</p>);
+};
