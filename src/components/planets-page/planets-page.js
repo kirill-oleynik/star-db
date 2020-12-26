c22100b842a32017bd4e3288942fddfb5acb126a
kirilloleynik <kirill.olejnik@gmail.com>
2020-12-26 10:40:37 +0200

    81. [done]

--- src/components/planets-page/planets-page.js
+++ src/components/planets-page/planets-page.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './planets-page.css';
 import {PlanetList,PlanetDetails} from '../sw-components';
-// import SwapiService from '../../services/swapi-service';
 import ItemField from '../item-field';
 import Row from '../row';
 import ItemDetails from '../item-details';
 
 export default class PlanetsPage extends Component{
-  // api = new SwapiService();
   state = {
     selectedItemId: null
   };
@@ -24,17 +22,7 @@ export default class PlanetsPage extends Component{
         onSelectItem={this.onSelectItem}
         renderLabel={this.renderLabel}
         entity='Planet'/>;
-    const itemDetails = <PlanetDetails itemId={5} />;
-    // const itemDetails =  <ItemDetails
-    // itemId={selectedItemId}
-    // getData={this.api.getPlanet}
-    // getImageUrl={this.api.getPlanetImage}>
-    //   <ItemField field={'population'} label={'Population'} />
-    //   <ItemField field={'diameter'} label={'Diameter'} />
-    //   <ItemField field={'rotationPeriod'} label={'Rotation Period'} />
-    //   </ItemDetails>
-
-
+    const itemDetails = <PlanetDetails itemId={this.state.selectedItemId} />;
     return(
     <Row
       left={itemList}
--- src/components/starships-page/starships-page.js
+++ src/components/starships-page/starships-page.js
@@ -2,12 +2,10 @@ import React, {Component} from 'react';
 import './starships-page.css';
 import {StarshipList,StarshipDetails} from '../sw-components';
 import ItemDetails from '../item-details';
-// import SwapiService from '../../services/swapi-service';
 import ItemField from '../item-field';
 import Row from '../row';
 
 export default class StarshipsPage extends Component {
-  // api = new SwapiService();
   state={
     slectedStarshipId: null
   };
@@ -24,21 +22,8 @@ export default class StarshipsPage extends Component {
     onSelectItem={this.onSelectItem}
     renderLabel={this.renderLabel}
     entity = 'Starship' />;
-    const itemDetails = <StarshipDetails itemId={10} />;
-    // const itemDetails =  <ItemDetails
-    // itemId={this.state.selectedItemId}
-    // getData={this.api.getStarship}
-    // getImageUrl={this.api.getStarshipImage}>
-    //     <ItemField field={'model'} label={'Model'} />
-    //     <ItemField field={'manufacturer'} label={'Manufacturer'} />
-    //     <ItemField field={'crew'} label={'Crew'} />
-    //     <ItemField field={'length'} label={'Length'} />
-    //     <ItemField field={'passengers'} label={'Passengers'} />
-    //     <ItemField field={'costInCredits'} label={'Cost in Credits'} />
-    //     <ItemField field={'cargoCapacity'} label={'Cargo Capacity'} />
-// </ItemDetails>
-
-    return(
+    const itemDetails = <StarshipDetails itemId={this.state.selectedItemId} />;
+      return(
     <Row
       left={itemList}
       right={itemDetails}
