5728aadb67e909cc9385883e01ed27119ef676fe
kirilloleynik <kirill.olejnik@gmail.com>
2021-01-15 10:38:54 +0200

    86. [wip] transform props in HOCs planet details

--- src/components/sw-components/person-details.js
+++ src/components/sw-components/person-details.js
@@ -3,15 +3,13 @@ import ItemDetails from '../item-details';
 import ItemField from '../item-field';
 import {withSwapiService} from '../hoc-helpers';
 
-const PersonDetails = (props) => {
-      return(
+const PersonDetails = (props) => (
   <ItemDetails {...props}>
       <ItemField field={'gender'} label={'Gender'} />
       <ItemField field={'birthYear'} label={'Birth Year'} />
       <ItemField field={'eyeColor'} label={'Eye Color'} />
       </ItemDetails>
       );
-};
 const mapMethodsToProps = (swapiService) => ({
     getData: swapiService.getPerson,
     getImageUrl: swapiService.getPersonImage
--- src/components/sw-components/planet-details.js
+++ src/components/sw-components/planet-details.js
@@ -2,24 +2,17 @@ import React from 'react';
 import ItemDetails from '../item-details';
 import ItemField from '../item-field';
 import {SwapiServiceConsumer} from '../swapi-service-context';
-const PlanetDetails = ({itemId}) => {
-  return(
-  <SwapiServiceConsumer>
-    {
-      ({ getPlanet,getPlanetImage }) => {
-        return(
-  <ItemDetails
-    itemId={itemId}
-    getData={getPlanet}
-    getImageUrl={getPlanetImage}>
+import {withSwapiService} from '../hoc-helpers';
+
+const PlanetDetails = (props) => (
+  <ItemDetails {...props}>
       <ItemField field={'population'} label={'Population'} />
       <ItemField field={'diameter'} label={'Diameter'} />
       <ItemField field={'rotationPeriod'} label={'Rotation Period'} />
       </ItemDetails>
         );
-      }
-    }
-  </SwapiServiceConsumer>
-  );
-};
-export default PlanetDetails;
+const mapMethodsToProps = (swapiService) => ({
+  getData: swapiService.getPlanet,
+  getImageUrl: swapiService.getPlanetImage
+});
+export default withSwapiService(PlanetDetails,mapMethodsToProps);
